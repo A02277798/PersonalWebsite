cfd0ee084049c848ad19d59b7c224b6fcceacda4
Ubuntu <ubuntu@ip-172-31-28-239.us-west-2.compute.internal>
2024-02-26 18:47:29 +0000

    Updated Resume

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template
-from datetime import date
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    today = todays_date()
-    return render_template("index.html", the_date = today)
+    return render_template("index.html")
     
 @app.route("/about")
 def about_me():
     return render_template("about_me.html")
-    
-def todays_date():
-    today = date.today()
-    str_date = today.strftime("%B %d, %Y")
-    return "The day of today is " + str_date + " :) "
